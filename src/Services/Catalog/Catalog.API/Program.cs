0ad44baabf4694490ecffe2800125f1384dd0355
vasilicapopescu <vasy.kr7@gmail.com>
2024-08-30 01:07:41 +0300

    added_marten_library; added_postgresql_sql

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -9,9 +9,14 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
 
 app.MapCarter();
 
